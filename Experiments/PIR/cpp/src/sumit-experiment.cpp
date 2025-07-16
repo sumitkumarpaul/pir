fa3e60c48b87e69c4e1313cf1098e1087fda3950
sumitkumarpaul <sumitpaul2310@gmail.com>
2025-07-16 16:39:58 -0400

    - OpenFHE is now used for homomorphic computations. - Experimentations with homomorphic computatoins are going on.

--- Experiments/PIR/cpp/src/sumit-experiment.cpp
+++ Experiments/PIR/cpp/src/sumit-experiment.cpp
@@ -1,12 +1,42 @@
-#include <chrono>
-#include <assert.h>
-#include <stdio.h>
-#include "fss-common.h"
-#include "fss-server.h"
-#include "fss-client.h"
+//==================================================================================
+// BSD 2-Clause License
+//
+// Copyright (c) 2014-2022, NJIT, Duality Technologies Inc. and other contributors
+//
+// All rights reserved.
+//
+// Author TPOC: contact@openfhe.org
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//==================================================================================
+
+/*
+  Example of a computation circuit of depth 3
+  BGVrns demo for a homomorphic multiplication of depth 6 and three different approaches for depth-3 multiplications
+ */
 
 #define PROFILE
 
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -14,103 +44,6 @@
 #include "openfhe.h"
 
 using namespace lbcrypto;
-
-#define N 10000 // Size of the database, can be adjusted as needed
-// The value of N wiil determine the bitlength during the client initialization
-#define BITS 14 
-// And number of bits determine the evalution time drastically
-static u_int64_t DB[N];
-
-// Client asks for the element located at location i, where 0 <= i < N 
-int PIR_Experiment(int I)
-{
-    // Set up variables
-    Fss fClient, fServer;
-    ServerKeyEq k0;
-    ServerKeyEq k1;
-
-    // Initialize the database
-    for(size_t i=0; i<N; i++) {
-        DB[i] = i*10000; // Each location stores a multiple of 10 of its index
-    }  
-
-    auto t_begin = std::chrono::high_resolution_clock::now();
-
-    // Initialize client, use 32 bits in domain as example
-    initializeClient(&fClient, BITS, 2); // If bit length is not set properly, then incorrect answer will be returned
-    // bit length must be able to store ans0 and ans1
-    
-    auto t_initClient = std::chrono::high_resolution_clock::now();
-
-    // Equality FSS test
-    generateTreeEq(&fClient, &k0, &k1, I, 1);//So that the point function will evaluate as 1 at location i, and zero elsewhere
-
-    auto t_keyGen = std::chrono::high_resolution_clock::now();    
-    
-    // Initialize server
-    initializeServer(&fServer, &fClient);
-
-    auto t_initServer = std::chrono::high_resolution_clock::now();
-
-    mpz_class ans0, ans1, fin;
-    ans0 = 0;
-    ans1 = 0;
-
-    // Server 0, evalute this for all i in [0, N)
-    for(size_t i=0; i<N; i++) {
-        auto x = evaluateEq(&fServer, &k0, i);
-        ans0 += x * mpz_class(DB[i]); // Accumulate the value at each index
-    }
-
-    auto t_evalServer0 = std::chrono::high_resolution_clock::now();
-
-    // Server 1, evalute this for all i in [0, N)
-    for(size_t i=0; i<N; i++) {
-        auto x = evaluateEq(&fServer, &k1, i);
-        ans1 += x * mpz_class(DB[i]); // Accumulate the value at each index
-    }
-
-    fin = ans0 - ans1;
-    cout << "PIR Test: value of ans0: " << ans0 << ", value of ans1: " << ans1 << endl;
-    cout << "PIR Test: (it should be the value stored at DB[I]): " << fin << endl;
-    cout << "The value stored at DB[I] is: " << DB[I] << endl;
-
-    auto t_evalServer1 = std::chrono::high_resolution_clock::now();    
-    
-    if (fin != mpz_class(DB[I])) {
-        cout << "!!!!!!!!!!!!!!!!!!! PIR Test: Error, the value returned does not match the expected value !!!!!!!!!!!!!" << endl;
-        return 0; // Error
-    } else {
-        cout << "PIR Test: Success, the value returned matches the expected value!" << endl;
-    }    
-
-    auto t_end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Time to initialize client: " <<
-     std::chrono::duration<double, std::milli>(t_initClient - t_begin).count()
-     << " ms" << endl;
-
-    std::cout << "Time to generate keys: " <<
-     std::chrono::duration<double, std::milli>(t_keyGen - t_initClient).count()
-     << " ms" << endl;
-
-    std::cout << "Time to initialize server: " <<
-     std::chrono::duration<double, std::milli>(t_initServer - t_keyGen).count()
-     << " ms" << endl;
-
-    std::cout << "Time to evaluate by server 0 " <<
-     std::chrono::duration<double, std::milli>(t_evalServer0 - t_initServer).count()
-     << " ms" << endl;
-
-    std::cout << "Time to evaluate by server 1 " <<
-     std::chrono::duration<double, std::milli>(t_evalServer1 - t_evalServer0).count()
-     << " ms" << endl;
-
-    std::cout << "Total time: " <<
-     std::chrono::duration<double, std::milli>(t_end - t_begin).count()
-     << " ms" << endl;
-    return 1;
-}
 
 int OpenFHEBGVrns_example(){
     ////////////////////////////////////////////////////////////
@@ -269,11 +202,6 @@ int OpenFHEBGVrns_example(){
     return 0;
 }
 
-int main()
-{
-
-    //PIR_Experiment(N-1);
+int main(int argc, char* argv[]) {
     OpenFHEBGVrns_example();
-
-    return 0;
 }
